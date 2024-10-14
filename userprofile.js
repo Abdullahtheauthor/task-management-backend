3d9fa1078002aff2e69850a902879c6a355beba4
iBxso <bxsoedit@gmail.com>
2024-10-14 22:06:28 +0300

    userProfile

---
+++ userprofile.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateUser } from './redux/actions'; // Assuming you have an updateUser action
+
+const UserProfile = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  const [editableUser, setEditableUser] = useState(user);
+
+  const handleSave = () => {
+    dispatch(updateUser(editableUser));
+  };
+
+  return (
+    <div className="profile-container">
+      <h2>User Profile</h2>
+      <label>Name</label>
+      <input
+        type="text"
+        value={editableUser.name}
+        onChange={(e) =>
+          setEditableUser({ ...editableUser, name: e.target.value })
+        }
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        value={editableUser.email}
+        onChange={(e) =>
+          setEditableUser({ ...editableUser, email: e.target.value })
+        }
+      />
+      <button onClick={handleSave}>Save</button>
+    </div>
+  );
+};
+
+export default UserProfile;
---
+++ userslice.js
@@ -0,0 +1,20 @@
+const initialState = {
+    name: 'John Doe',
+    email: 'john@example.com',
+  };
+  
+  const userSlice = (state = initialState, action) => {
+    switch (action.type) {
+      case 'UPDATE_USER':
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  };
+  
+  export const updateUser = (user) => ({
+    type: 'UPDATE_USER',
+    payload: user,
+  });
+  
+  export default userSlice;
